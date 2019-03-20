84defa292801341e54e6b28de1182b859163f332
Zexi Han <zexihan@me.com>
2019-03-20 14:24:05 -0400

    created routes for descriptive data

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+from flask_cors import CORS
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+types = ["restaurants", "shopping", "nightlife", "expense", "host", "noise", "safety", "transit"]
+
+df_map = {}
+for type in types:
+    df_map[type] = pd.read_csv("data/" + type + ".csv")
+
+class DescriptiveData(Resource):
+    def get(self, type, col):
+        df = df_map[type]
+        res = {}
+        for i in range(df.shape[0]):
+            res[str(i)] = df.iloc[i][col]
+        return res
+        
+api.add_resource(DescriptiveData, '/api/<type>/<col>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
